dc635767bc5ea3e7f1a46bc2a67d6c8b0c44d681
mohansky <38560439+mohansky@users.noreply.github.com>
2022-02-05 19:55:19 +0530

    Add files via upload

---
+++ gatsby-browser.js
@@ -0,0 +1,20 @@
+ 
+// CUSTOM CSS
+import "./src/styles/style.scss"
+
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+//Animate
+import 'animate.css'
+
+// Click Carousel
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+//Animate On Scroll Library
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+AOS.init();
