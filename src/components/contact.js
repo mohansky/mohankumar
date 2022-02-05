d82749e00da359a51c3b9d7671e355710bc25c6a
mohansky <38560439+mohansky@users.noreply.github.com>
2022-02-06 00:01:05 +0530

    Update contact.js

--- src/components/contact.js
+++ src/components/contact.js
@@ -29,70 +29,67 @@ const Contact = () => {
   return (
     <section id="contact"> 
        <Container className="section-top"> 
-        <Row>
-          <Col md={4}>
-            <h4 className="text-end fw-bolder" data-aos="fade-right">Enough about me,<br/><span className="accent">let's talk</span> about you </h4>
-          </Col>
-          
-          <Col md={1}></Col>
-    
-          <Col md={7} data-aos="fade-left">
-
-            <Form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field">
-            <p className="hidden">
-              <label>
-                Dont fill this out if youre human: <input name="bot-field" />
-              </label>
-            </p>
             <Row>
-              <Col xs={12} md={6}>
-              <Form.Group className="mb-5" controlId="formName">
-                <Form.Label>Name</Form.Label>
-                <Form.Control type="name" name="name" placeholder="Name" /> 
-              </Form.Group>
+              <Col md={4}>
+                <h4 className="text-end fw-bolder" data-aos="fade-right">Enough about me,<br/><span className="accent">let's talk</span> about you </h4>
               </Col>
-              <Col xs={12} md={6}>
-              <Form.Group className="mb-5" controlId="formEmail">
-                <Form.Label>E-mail</Form.Label>
-                <Form.Control type="email" name="email" placeholder="E-mail" />
+          
+              <Col md={1}></Col>
+    
+              <Col md={7} data-aos="fade-left">
+
+                <Form name="contact" method="POST" data-netlify="true" action="/thankyou/#contact"netlify-honeypot="bot-field">
+                <p className="hidden">
+                  <label>
+                    Dont fill this out if youre human: <input name="bot-field" type="hidden" value="contact" />
+                  </label>
+                </p>
+                <Row>
+                  <Col xs={12} md={6}>
+                  <Form.Group className="mb-5" controlId="formName">
+                    <Form.Label>Name</Form.Label>
+                    <Form.Control type="name" name="name" placeholder="Name" required/> 
+                  </Form.Group>
+                  </Col>
+                  <Col xs={12} md={6}>
+                  <Form.Group className="mb-5" controlId="formEmail">
+                    <Form.Label>E-mail</Form.Label>
+                    <Form.Control type="email" name="email" placeholder="E-mail" required/>
+                  </Form.Group>
+                  </Col>
+                </Row>
+
+
+                <Col md={{ span: 6, offset: 1 }}>
+                <Form.Group className="mb-5" controlId="formCheckbox">
+                  <Form.Check
+                      className="mb-3"
+                      type="radio"
+                      label="I know exactly what sort of website I need and how it should look."
+                      name="option1"
+                      id="formRadios1"
+                    />
+                    <Form.Check
+                      className="mb-3"
+                      type="radio"
+                      label="I'm not sure what is feasible and only have a vague idea of what my website could to look like. "
+                      name="option1"
+                      id="formRadios2"
+                    />
+                    <Form.Check
+                      className="mb-3"
+                      type="radio"
+                      label="I already have a website, but I'm not happy with it and would like some changes."
+                      name="option1"
+                      id="formRadios3"
+                    />
+                </Form.Group>
+                </Col>
+
+              <Form.Group className="mb-5" controlId="formMessage">
+                <Form.Label>Message</Form.Label>
+                <Form.Control as="textarea" name="message" rows={3} required/>
               </Form.Group>
-              </Col>
-            </Row>
-
-
-            <Col md={{ span: 6, offset: 1 }}>
-            <Form.Group className="mb-5" controlId="formCheckbox">
-              <Form.Check
-                  className="mb-3"
-                  type="radio"
-                  label="I know exactly what sort of website I need and how it should look."
-                  name="option1"
-                  id="formRadios1"
-                />
-                <Form.Check
-                  className="mb-3"
-                  type="radio"
-                  label="I'm not sure what is feasible and only have a vague idea of what my website could to look like. "
-                  name="option1"
-                  id="formRadios2"
-                />
-                <Form.Check
-                  className="mb-3"
-                  type="radio"
-                  label="I already have a website, but I'm not happy with it and would like some changes."
-                  name="option1"
-                  id="formRadios3"
-                />
-            </Form.Group>
-            </Col>
-
-            <Form.Group className="mb-5" controlId="formMessage">
-              <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" name="message" rows={3} />
-            </Form.Group>
-
-            
-
 
               <Button className="float-end btn-form" type="submit">
                 SUBMIT
